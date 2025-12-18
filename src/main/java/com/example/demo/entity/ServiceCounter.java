0e635b1af6720a8a08153f46c6f0337998b43b02
bala-amypo <bala@amypo.in>
2025-12-18 09:23:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/ServiceCounter.java
+++ src/main/java/com/example/demo/entity/ServiceCounter.java
@@ -1,5 +1,20 @@
+package com.example.demo.entity;
 
-
+import jakarta.persistence.*;
+@Entity
 public class ServiceCounter{
-    
+    private Long id;
+    private String counterName;
+    private String department;
+    private Boolean isActive;
+
+    public ServiceCounter(){
+
+    }
+
+    public ServiceCounter(Long id,String counterName,String department,Boolean isActive){
+        this.counterName=counterName;
+        this.department=department;
+        this.isActive=isActive;
+    }
 }
