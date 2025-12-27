fd3f24493841758d4860ef92fac05472ac7805c6
bala-amypo <bala@amypo.in>
2025-12-27 05:30:09 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,19 +1,19 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig{
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenApi customOpenAPI(){
         return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Queue Management System")
-                        .version("1.0"));
+        .servers(List.of(
+            new Server().url("https://9052.408procr.amypo.ai/")    
+        ));
     }
 }
-
